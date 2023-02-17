c7e795b9bd475989cb71a99940b4f5763a0189fa
Brydon <121854370+existancepy@users.noreply.github.com>
2023-02-17 22:12:08 +0800

    Update calibrate_hive.py

--- calibrate_hive.py
+++ calibrate_hive.py
@@ -1,3 +1,25 @@
+import pyautogui as pag
+import time
+import os
+import tkinter
+import loadsettings
+import move
+from webhook import webhook
+import imagesearch
+
+def loadSave():
+    info = {}
+    with open('save.txt') as f:
+        lines = f.read().split("\n")
+    f.close()
+    for s in lines:
+        l = s.replace(" ","").split(":")
+        if l[1].isdigit():
+            l[1] = int(l[1])
+        info[l[0]] = l[1]
+    return info
+
+
 def calibrate():
     cmd = """
         osascript -e 'activate application "Roblox"' 
