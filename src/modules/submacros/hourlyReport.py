0aef3fd1727fedcc1fc12ae697a2df5073d4ba24
exihs <121854370+existancepy@users.noreply.github.com>
2024-10-05 15:20:41 +0800

    load planters into hourly report

--- src/modules/submacros/hourlyReport.py
+++ src/modules/submacros/hourlyReport.py
@@ -5,7 +5,7 @@ import cv2
 from modules.misc.settingsManager import readSettingsFile
 import math
 import time
-
+import ast
 hti = Html2Image(size=(1900, 770))
 
 def millify(n):
@@ -56,12 +56,18 @@ def generateHourlyReport():
         #get the hourly report data
         hourlyReportData = {**readSettingsFile("data/user/hourly_report_main.txt"), **readSettingsFile("data/user/hourly_report_bg.txt")}
 
+        #get planter data
+        with open("./data/user/manualplanters.txt", "r") as f:
+            planterData = f.read()
+        f.close()
+
         #calculate honey/min
         honeyPerMin = [0]
         prevHoney = hourlyReportData["honey_per_min"][0]
         for x in hourlyReportData["honey_per_min"][1:]:
             honeyPerMin.append(x-prevHoney)
             prevHoney = x
+
         #replace the contents of the html
         replaceDict = {
             'src="a': f'src="{hourlyReportDir}/a',
@@ -75,12 +81,22 @@ def generateHourlyReport():
             "-sessTime": display_time(time.time()-hourlyReportData["start_time"], ['d','h','m']),
             "var honeyPerMin = []": f'var honeyPerMin = {honeyPerMin}',
             "var backpackPerMin = []": f'var backpackPerMin = {hourlyReportData["backpack_per_min"]}',
-            "const taskTimes = []": f'const taskTimes = [{hourlyReportData["gathering_time"]}, {hourlyReportData["converting_time"]}, {hourlyReportData["bug_run_time"]}, {hourlyReportData["misc_time"]}]'
+            "const taskTimes = []": f'const taskTimes = [{hourlyReportData["gathering_time"]}, {hourlyReportData["converting_time"]}, {hourlyReportData["bug_run_time"]}, {hourlyReportData["misc_time"]}]',
         }
+
+        if planterData:
+            planterData = ast.literal_eval(planterData)
+            print("hi")
+            planterReplaceDict = {
+                "const planterNames = []": f'const planterNames = {planterData["planters"]}', 
+                "const planterTimes = []": f'const planterTimes = {[planterData["harvestTime"]-time.time()]*3}',
+                "const planterFields = []": f'const planterFields = {planterData["fields"]}',
+            }
+            replaceDict = {**replaceDict, **planterReplaceDict}
         for k,v in replaceDict.items():
             htmlString = htmlString.replace(k,str(v))
         #save the html as an image
-        print(htmlString)
+        if "2" in page: print(htmlString)
         hti.screenshot(html_str=htmlString, save_as=f"{pageName}.png")
         #open the image in pillow
         pageImages.append(cv2.imread(f"{pageName}.png"))
