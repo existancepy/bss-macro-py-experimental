541176212428851437546474bb2bee80a31eac62
sevmanash <kymani359@gmail.com>
2025-04-16 23:00:02 -0400

    Add files via upload

---
+++ settings/paths/vic/kill_vic/mountain top.py
@@ -0,0 +1,12 @@
+
+
+side = 2
+back = 0.8
+self.keyboard.walk("w",back)
+self.keyboard.walk("a",side)
+time.sleep(0.7)
+self.keyboard.walk("s",back)
+self.keyboard.walk("d",side)
+time.sleep(0.7)
+
+    
