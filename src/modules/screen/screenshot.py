05e7f2f09f090dc1e85b12b82cb1cdba9a6ff204
existancepy <121854370+existancepy@users.noreply.github.com>
2025-05-29 17:17:45 +0800

    Update screenshot.py

--- src/modules/screen/screenshot.py
+++ src/modules/screen/screenshot.py
@@ -4,6 +4,8 @@ import mss.tools
 import time
 import pyautogui as pag
 import numpy as np
+import time
+
 mw, mh = pag.size()
 #returns an NP array, useful for cv2
 def mssScreenshotNP(x,y,w,h, save = False):
@@ -17,12 +19,16 @@ def mssScreenshotNP(x,y,w,h, save = False):
 
 
 def mssScreenshot(x=0,y=0,w=mw,h=mh, save = False):
+    st = time.time()
+    print(f"x:{x}, y:{y}, w: {w}, h:{h}")
     with mss.mss() as sct:
         # The screen part to capture
         monitor = {"left": int(x), "top": int(y), "width": int(w), "height": int(h)}
         # Grab the data and convert to pillow img
         sct_img = sct.grab(monitor)
+        print(f"grabbed monitor: {time.time()-st}")
         img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
+        print(f"converted image: {time.time()-st}")
         if save: mss.tools.to_png(sct_img.rgb, sct_img.size, output=f"screen-{time.time()}.png")
         return img
 
