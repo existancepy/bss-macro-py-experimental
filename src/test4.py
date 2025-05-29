b0e1314f59a0361c777519ec73a6bf2d8770ef76
existancepy <121854370+existancepy@users.noreply.github.com>
2025-05-29 14:28:06 +0800

    Update test4.py

--- src/test4.py
+++ src/test4.py
@@ -5,22 +5,9 @@ import imagehash
 import time
 import pyautogui as pag
 from modules.misc.imageManipulation import adjustImage
+from modules.screen.imageSearch import locateImageOnScreen
 
 mw, mh = pag.size()
-def templateMatch(smallImg, bigImg):
-    res = cv2.matchTemplate(bigImg, smallImg, cv2.TM_CCOEFF_NORMED)
-    return cv2.minMaxLoc(res)
 
-st = time.time()
-
-target = adjustImage("./images/menu", "mmopen", "retina")
-print(f"Adjusted image: {time.time()-st}")
-screen = mssScreenshot(0,0,mw,mh)
-print(f"Took screenshot: {time.time()-st}")
-
-screen = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
-print(f"Converted to BGR: {time.time()-st}")
-
-templateMatch(target, screen)
-print(f"Template match: {time.time()-st}")
-print("done")
+yesImg = adjustImage("./images/menu", "yes", "retina")
+locateImageOnScreen(yesImg,.mw/3.2,mh/2.3,mw/2.5,mh/3.4, threshold)
