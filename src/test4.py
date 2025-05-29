f27d3251ab100e499cb69792b773316c49aa6808
existancepy <121854370+existancepy@users.noreply.github.com>
2025-05-29 13:04:11 +0800

    Update test4.py

--- src/test4.py
+++ src/test4.py
@@ -4,6 +4,8 @@ import numpy as np
 import imagehash
 import time
 import pyautogui as pag
+from modules.misc.imageManipulation import adjustImage
+
 mw, mh = pag.size()
 def templateMatch(smallImg, bigImg):
     res = cv2.matchTemplate(bigImg, smallImg, cv2.TM_CCOEFF_NORMED)
@@ -11,13 +13,14 @@ def templateMatch(smallImg, bigImg):
 
 st = time.time()
 
+target = adjustImage("./images/menu", "mmopen", "retina")
+print(f"Adjusted image: {time.time()-st}")
 screen = mssScreenshot(0,0,mw,mh)
 print(f"Took screenshot: {time.time()-st}")
 
-target = cv2.imread("images/menu/ebutton-retina.png", 0)
 screen = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
-print(f"Converted to BGR and got target image: {time.time()-st}")
+print(f"Converted to BGR: {time.time()-st}")
 
-templateMatch(screen, target)
+templateMatch(target, screen)
 print(f"Template match: {time.time()-st}")
 print("done")
